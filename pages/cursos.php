576b883df56735d8bdbaaf9f0571317e6408b415
FolksNP <110176923+FolksNP@users.noreply.github.com>
2023-03-13 19:54:44 -0300

    Add files via upload

---
+++ pages/cursos.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/css/main.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <?php include('header.php'); ?>
+    <div class="tudoCarrosselCursos">
+        <div class="continuarAssistindo">
+<div class="rolagemHorizontal">
+        <p>Continue Aprendendo</p>
+    <div class="navegacaoCursos">
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+  </div>
+  </div>
+  </div>
+  <div class="recomendados">
+  <div class="rolagemHorizontal">
+        <p>Em alta</p>
+    <div class="navegacaoCursos">
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+  </div>
+  </div>
+  <div class="rolagemHorizontal">
+        <p>Com base no seu interesse</p>
+    <div class="navegacaoCursos">
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+  </div>
+  </div>
+  <div class="rolagemHorizontal">
+        <p>Novidades fresquinhas</p>
+    <div class="navegacaoCursos">
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+    <div class="imgDescCurso"><img src="../imgs/binladen.jpg"><p>ta tranquilo ta favorável </p></div>
+  </div>
+  </div>
+</div>
+<?php include('footer.php'); ?>
+</body>
+</html>
---
+++ pages/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    
+</head>
+
+<body>
+
+    <div class="navbarAzul">
+        <div class="logoPesquisa">
+            <div class="logoImg"><img src="../imgs/logoTCC.png" alt=""><p>Patrono Neves</p></div>  
+            <p><input type="text" placeholder="Digite aqui sua pesquisa"></p>
+            <p class="perfilImg"><img src="../imgs/rony.jpg" alt=""></p>
+        </div>
+
+        <div class="catNavbar"> 
+            <ul>
+                <li>Inicio</li>
+                <li>Cursos</li>
+                <li>Sobre nós</li>
+                <li>Suporte</li>
+            </ul>
+                
+        </div>
+    </div>    
+
+</body>
+
+</html>
