31e6b4624da429f436ea181550130dc5d4b51994
FolksNP <110176923+FolksNP@users.noreply.github.com>
2023-03-19 11:18:58 -0300

    Delete pages directory

--- pages/logoff.php
+++
@@ -1,8 +0,0 @@
-<?php
-    @session_start();
-        $SESSION['login'] = false;
-        session_destroy();
-        unset($_SESSION['login']);
-        session_destroy();
-        header("location:index.php");
-?>
