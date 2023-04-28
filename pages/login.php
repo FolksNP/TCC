e8b6e6cf3e312d1846d19092c8a8806a67b6058b
FolksNP <Felipe70103@gmail.com>
2023-04-28 20:11:29 -0300

    projeto melhorado

--- pages/login.php
+++ pages/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/css/main.css">
-    <title>Document</title>
+    <title>Login - Patrono Neves</title>
 </head>
 
 
