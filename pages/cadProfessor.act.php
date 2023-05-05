5d146c6d9248eea32b44c4b9f85dee5195bf4ccf
FolksNP <felipe70103@gmail.com>
2023-05-05 17:12:19 -0300

    cadProfessor e cadProfessor.act

---
+++ pages/cadProfessor.act.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require('connect.php');
+
+$codigoProf = $_SESSION['cod'];
+
+extract($_POST);
+
+$buscaProfessor = mysqli_query($con, "SELECT * FROM `professores`");
+var_dump($buscaProfessor);
+$professor = mysqli_fetch_array($buscaProfessor);
+
+if(mysqli_query($con, "INSERT INTO `professores` ( `codProfessor`,`cpf`,`numAgencia`, `numConta`) 
+VALUES ('$codigoProf', '$cpf', '$numAgencia', '$numConta');")){
+        $msg = "Seja bem-vindo ao time!";
+
+} else{
+    $msg = "Não foi possível realizar seu cadastro!";
+}
+
+var_dump($con);
+echo $msg;
+$_SESSION['msg'] = $msg;
+//header("location:inicial.php");
+?>
---
+++ pages/cadProfessor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="../styles/css/main.css"> -->
+    <title>Junte-se a nós!</title>
+</head>
+<body>
+    
+    <form action="cadProfessor.act.php" method="post" enctype="multipart/form-data">
+        <p>CPF:</p>
+        <input type="text" name="cpf" id="">
+
+        <p>Número da sua Agência</p>
+        <input type="text" name="numAgencia" id="">
+
+        <p>Número da Conta</p>
+        <input type="text" name="numConta" id=""><br><br>
+
+        <input type="submit" value="Concluir">
+        8
+    </form>
+
+</body>
+</html>
