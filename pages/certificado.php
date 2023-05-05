55c9016406e6d12fa3339df6c045092b00440332
FolksNP <felipe70103@gmail.com>
2023-05-05 15:17:28 -0300

    certificado feito

--- pages/certificado.php
+++ pages/certificado.php
@@ -5,18 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificado Patrono Neves</title>
+    <link rel="stylesheet" href="../styles/css/main.css">
 </head>
 <body>
-    <div class="principal">
-        <div class="patrono-neves">
 
+    <?php include('header.php')?>
+
+    <div class="principal-certificado">
+        <div class="patrono-neves-certificado">
+            <div class="caixa-pn">
+                <h1 class="pn-letter">P</h1>
+                <h1 class="pn-letter">A</h1>
+                <h1 class="pn-letter">T</h1>
+                <h1 class="pn-letter">R</h1>
+                <h1 class="pn-letter">O</h1>
+                <h1 class="pn-letter">N</h1>
+                <h1 class="pn-letter">O</h1>
+                <br>
+                
+                <h1 class="pn-letter">N</h1>
+                <h1 class="pn-letter">E</h1>
+                <h1 class="pn-letter">V</h1>
+                <h1 class="pn-letter">E</h1>
+                <h1 class="pn-letter">S</h1>
+            </div>        
+        </div>
+        <div class="caixa-linha">
+            <img id="detalhe" src="../imgs/certificado-detalhe.png" alt="">
         </div>
         <div class="conteudo-principal">
-
+            <h1 id="certi">CERTIFICADO</h1>
+            <h3 id="conc"><i>DE CONCLUSÃO DE CURSO</i></h3>
         </div>
         <div class="conteudo-lateral-direito">
-            
+            <div class="bloco-superior"></div>
+            <div class="logo">
+                <img src="../imgs/logoTCC.png" alt="">
+            </div>
+            <div class="bloco-inferior"></div>
         </div>
     </div>
+
+    <?php include('footer.php')?>
 </body>
 </html>
