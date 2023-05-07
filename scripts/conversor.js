3b69daa88ebfde5b736f1a55803a0f41379b1525
FolksNP <Felipe70103@gmail.com>
2023-05-07 17:12:29 -0300

    conversor pdf - está quase

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,10 +1,11 @@
 function downloadPDF() {
     const item = document.querySelector(".principal-certificado")
     var opt = {
-        margin: 0,
+        margin: [1, 0, 0, 0],
         filename: "certificado-patrono-neves.pdf",
-        html2canvas: {scale: 2 },
-        jsPDF: {unit: "in", format: "letter", orientation: "landscape"},
+        image: {type: 'png', quality: 0.98},
+        html2canvas: {scale: 4 },
+        jsPDF: {unit: "in", format: "a3", orientation: "landscape"},
     };
 
     html2pdf().set(opt).from(item).save();
