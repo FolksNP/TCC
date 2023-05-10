e1ab4dc5e9b84fd46041d74cfabe04e96aea2d93
FolksNP <felipe70103@gmail.com>
2023-05-10 15:26:58 -0300

    tela video melhorado

--- pages/tela.video.php
+++ pages/tela.video.php
@@ -68,10 +68,10 @@
                 <iframe width="1260" height="710" src="https://www.youtube.com/embed/8E-7iPhM3CI" title="Aula1 ‐ Introdução a Programação Geral" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <div class="links-inferiores">
                     <ul>
-                        <li>Descrição</li>
-                        <li>Comentários</li>
-                        <li>Arquivos</li>
-                        <li>Referências</li>
+                        <a href="" id="links"><li>Descrição</li></a>
+                        <a href="" id="links"><li>Comentários</li></a>
+                        <a href="" id="links"><li>Arquivos</li></a>
+                        <a href="" id="links"><li>Referências</li></a>
                     </ul>
                 </div>
                 <div class="conteudo-inferior">
