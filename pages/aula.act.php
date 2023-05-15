1be9683b373fb7f4e59a73a354253fa85a86e3ee
PedroMirandaro <pedro.mirandaro@gmail.com>
2023-05-15 16:52:50 -0300

    videos comittados

--- pages/aula.act.php
+++ pages/aula.act.php
@@ -9,12 +9,12 @@ var_dump($_FILES);
     $arqVideo = "../videos/".md5(time().$videoAula['size']). ".mp4";
     move_uploaded_file($videoAula['tmp_name'],$arqVideo);
    
-   if(mysqli_query($con, "INSERT INTO `aulas` ( `codAula`,`duracao`, `video`, `referencias`, `codCurso`) 
-   VALUES (NULL, 5, '$arqVideo','Warner Bros', '$codCurso');")){
+    if(mysqli_query($con, "INSERT INTO `aulas` ( `codAula`,`duracao`, `video`, `referencias`, `codCurso`) 
+    VALUES (NULL, 5, '$arqVideo','Warner Bros', '$codCurso');")){
            $msg = "aula cadastrada";
            
            
-       } else{
+        } else{
            $msg = "Erro ao se cadastrar";
         }
         echo $msg;
